17c3312040b755450e398b6950a50e7c29a135bd
Fred <domaine75@gmail.com>
2025-03-10 17:26:16 +0100

    add controls

--- PostgresqlDatabaseEqualizer/MainWindow.xaml.cs
+++ PostgresqlDatabaseEqualizer/MainWindow.xaml.cs
@@ -1,21 +1,33 @@
 ﻿using System;
-using System.IO; 
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Windows;
 
 namespace PostgresqlDatabaseEqualizer
 {
   /// <summary>
-  /// Logique d'interaction pour MainWindow.xaml
+  /// Interaction Logic for MainWindow.xaml
   /// </summary>
   public partial class MainWindow: Window
   {
     private bool _sourceConnectionOK;
     private bool _destinationConnectionOK;
+    private string _schemaFilename;
+    private List<string> _allSchemas;
 
     public MainWindow()
     {
       InitializeComponent();
-      CheckFilePresence("schemas.txt");
+      _schemaFilename = "schemas.txt";
+      CheckFilePresence(_schemaFilename);
+      LoadConfigurationFileComboBox();
+    }
+
+    private void LoadConfigurationFileComboBox()
+    {
+      cboConfigurationFile.Items.Clear();
+      cboConfigurationFile.Items.Add(_schemaFilename);
     }
 
     private void ButtonConnectionTarget_Click(object sender, RoutedEventArgs e)
@@ -26,17 +38,33 @@ namespace PostgresqlDatabaseEqualizer
       //MessageBox.Show("Please enter a connection string", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
     }
 
-    private static void CheckFilePresence(string fileName)
+    private void CheckFilePresence(string fileName)
     {
       string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
       if (File.Exists(filePath))
       {
-        MessageBox.Show($"{fileName} est présent.");
+        // read the content
+        _allSchemas = File.ReadAllLines(filePath).ToList();
       }
       else
       {
-        MessageBox.Show($"{fileName} est manquant.");
+        // create the file
+        File.Create(filePath).Close();
+        // write the content
+        File.WriteAllText(filePath, "schema1");
+        // add another schema
+        File.AppendAllText(filePath, Environment.NewLine + "schema2");
+        // add another schema
+        File.AppendAllText(filePath, Environment.NewLine + "schema3");
+        // add another schema
+        File.AppendAllText(filePath, Environment.NewLine + "schema4");
+        _allSchemas = File.ReadAllLines(filePath).ToList();
       }
+    }
+
+    private void BtnSaveConfigurationFile_Click(object sender, RoutedEventArgs e)
+    {
+
     }
   }
 }
